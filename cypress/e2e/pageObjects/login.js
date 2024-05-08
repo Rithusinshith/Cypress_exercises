4aab3eea91aea246bda1b85ba2b02166a66b18c5
rithusinshith <rithu.raj@3plearning.com>
2024-05-08 15:21:45 +1000

    added Login Tc

--- cypress/e2e/pageObjects/login.js
+++ cypress/e2e/pageObjects/login.js
@@ -1,6 +1,6 @@
 const logo = '.logo'
-const login = 'fa-lock'
-const loginEmail = '[data-qa="login-email]'
+const login = '.navbar-nav >li:nth-child(4) > a'
+const loginEmail = '[data-qa="login-email"]'
 const loginPassword ='[data-qa="login-password"]'
 const loginBtn = '[data-qa="login-button"]'
 const loginUser = '.fa-user'
