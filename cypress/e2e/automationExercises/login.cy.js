0e89d8df80ac82f71c73e083444367f0cc58b049
rithusinshith <rithu.raj@3plearning.com>
2024-07-24 13:57:28 +1000

    check

--- cypress/e2e/automationExercises/login.cy.js
+++ cypress/e2e/automationExercises/login.cy.js
@@ -2,13 +2,13 @@ import logInPage from "../pageObjects/login";
 
 describe('As a registered user, I can login into the website', () => {
   
-    it('Verify that a non registered user can register to the website', () => {
+    it('Verify that a registered user can login to the website', () => {
 
         logInPage.homePage()
         logInPage.navigateToLoginPage()
         logInPage.enterUserCreds()
         logInPage.clickLoginButton()
-        logInPage.validateuserLoggedIn
+        logInPage.validateuserLoggedIn()
 
 
     })
